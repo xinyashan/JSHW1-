407fb00527f6e24b70c8b891dea72901e7fed1f2
xinyashan <50379822+xinyashan@users.noreply.github.com>
2019-05-16 07:59:48 -0500

    Add files via upload

---
+++ JSHW1/creativestory.js
@@ -0,0 +1,22 @@
+var hero = 'Warrior';
+if(hero === 'Mage') { console.log('Hello, powerful ' + hero); }
+else if (hero === 'Warrior') { console.log('Hello, great ' + hero); }
+else { console.log('You did not choose!'); }
+
+var fairy = 'heroine';
+if(fairy === 'heroines') { console.log('Hello, powerful ' + hero); }
+else if (fairy === 'my chosen heroine.') { console.log('This, has super healing powers that can last for more than five minutes! Use your items wisely please,' + fairy); }
+else { console.log('You did not choose!'); }
+
+var food = 'sandwhiches';
+if(food === 'snacks') { console.log('Has bonus effects' + food); }
+else if (food === 'foodie') { console.log('This, sandwhich has healing powers! It heals +100 towards one party member! ' + hero); }
+else { console.log('You have selected this yummy sandwhich! There is 0 left now'); }
+
+
+
+
+
+
+
+
